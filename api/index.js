1f7417489591ceead37b71314258ca2f44b3dcc6
noahmilstein <casanoah@gmail.com>
2018-02-18 17:56:55 -0500

    fixed a typo

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-// this will eventuall be our API for sending and pulling info from mongodb, twitter, and elsewhere
+// this will eventually be our API for sending and pulling info from mongodb, twitter, and elsewhere
 router.get('/', (req, res) => {
   res.send("It\'s ALIVE!")
 })
